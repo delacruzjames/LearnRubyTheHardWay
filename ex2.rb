a84fe4c3d6f827d249a47c7c1c3e78c768705c4d
delacruzjames <delacruzjamesmartin@gmail.com>
2012-11-05 22:26:17 +0800

    ex2.rb comment and reverse

---
+++ ex2.rb
@@ -0,0 +1,9 @@
+# a comment, this is so you xan read your program later.
+# anything after the # is ignore by ruby
+
+puts "I could have code like this." # and the comment after is ignore
+
+# you can also use a comment to "disable" or comment out a piece of code:
+# puts "This wont run."
+puts "this will run"
+puts "this will run".reverse
