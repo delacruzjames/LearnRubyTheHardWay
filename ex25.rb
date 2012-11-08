554b0e0eba62b48c9dd1f8e5bb3f1e9a4eb13288
delacruzjames <delacruzjamesmartin@gmail.com>
2012-11-08 15:52:32 +0800

    ex25.rb is working well will be right back at the exam.rb to finish the test

--- ex25.rb
+++ ex25.rb
@@ -1,30 +1,56 @@
 module Ex25
 	def self.break_words(stuff)
-		# this function will break up words for us
+		# this for breaking words
 		words = stuff.split(' ')
 		words
 	end
 
-	def self.sort_words(words)
-		# Sort the words
+	def self.sorted_words(words)
+		# this function is for sort the words
 		words.sort()
 	end
 
 	def self.print_first_word(words)
-		# print the first word and shifts the others down by one
-		word = words.first
-		puts word
+		# this is for print the first word of sentence use by '.first' && '.shift'
+		first_word = words.first()
+		shift_word = words.shift()
+		puts first_word
+		puts shift_word
 	end
 
 	def self.print_last_word(words)
-		# print the first word and shifts the others down by one
-		word = words.last
-		puts word
+		# this is for print the last word if sentence use  by '.last' && '.pop'
+		last_word = words.last()
+		pop_word = words.pop()
+		puts last_word
+		puts pop_word
 	end
 
 	def self.sort_sentence(sentence)
-		# takes in a full sentences and returns the sorted words.
+		# takes in a full sentences and returns the sorted words
 		words = break_words(sentence)
-		sort_words(words)
+		sorted_words(words)
 	end
 end
+
+sentence = "All good things come to those who waits."
+
+words = Ex25.break_words(sentence)
+sorted_words = Ex25.sorted_words(words)
+
+puts "-" * 30
+puts words
+puts "-" * 30
+puts # blank
+puts "-" * 30
+puts sorted_words
+puts "-" * 30
+
+Ex25.print_last_word(words)
+Ex25.print_first_word(words)
+puts "*" * 30
+sort_sentence = Ex25.sort_sentence(sentence)
+puts sort_sentence
+
+
+
