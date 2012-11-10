6f192f5c891976f97927463479e35dc06f6cc8c0
delacruzjames <delacruzjamesmartin@gmail.com>
2012-11-10 11:23:58 +0800

    modify the 38.rb and its cool

--- ex38.rb
+++ ex38.rb
@@ -1,8 +1,23 @@
-# ten_things = "Apples Oranges Crows Telephone Light Sugar"
+ten_things = "Apples Oranges Crows Telephone Light Sugar"
 
-# puts "Wait there's not 10 things in that list"
+puts "Wait there's not 10 things in that list"
 
-# stuff = ten_things.split(' ')
-# more_stuff = %w(Day Night Song Frisbee Corn Banana Girl Boy)
+stuff = ten_things.split(' ')
+more_stuff = %w(Day Night Song Frisbee Corn Banana Girl Boy)
 
-puts stuff[1]
+while stuff.length != 10
+	next_one = more_stuff.pop()
+	puts "Adding: #{next_one}"
+	stuff.push(next_one)
+	puts "there's #{stuff.length} items now."
+end
+
+puts "there we go #{stuff}"
+
+puts "lets do some things with stuff"
+
+puts stuff[1] # 2nd on the array ten_things
+puts stuff[-1] # last of the array
+puts stuff.pop() # last of the array
+puts stuff.join(' ') # nothing special
+puts stuff.values_at(3,5).join('#')
